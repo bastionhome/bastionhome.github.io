51d52029217ee83a2e440005746fbfa61694e446
Ben Christel <ben.christel@gmail.com>
2023-10-01 15:19:47 -0700

    Format

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import {h} from "preact"
 import "./app.css"
-import { Podium } from "./Podium"
+import {Podium} from "./Podium"
 
 export function App() {
-  return <div class="app">
-    <div class="page">
-      <div class="header">
-        <h1>Bastion</h1>
+  return (
+    <div class="app">
+      <div class="page">
+        <div class="header">
+          <h1>Bastion</h1>
+        </div>
+        <nav class="menu">
+          <a href="https://benchristel.github.io/tv">TV</a>
+          <a href="https://benchristel.github.io/recipes">Recipes</a>
+          <a href="https://github.com/benchristel/benchristel.github.io/wiki">
+            Bliki
+          </a>
+          <a href="https://mail.google.com">Gmail</a>
+          <a href="https://benchristel.github.io/call">Call</a>
+        </nav>
+        <Podium />
       </div>
-      <nav class="menu">
-        <a href="https://benchristel.github.io/tv">TV</a>
-        <a href="https://benchristel.github.io/recipes">Recipes</a>
-        <a href="https://github.com/benchristel/benchristel.github.io/wiki">Bliki</a>
-        <a href="https://mail.google.com">Gmail</a>
-        <a href="https://benchristel.github.io/call">Call</a>
-      </nav>
-      <Podium />
     </div>
-  </div>
+  )
 }
--- src/Podium.tsx
+++ src/Podium.tsx
@@ -2,33 +2,83 @@ import {h, Fragment} from "preact"
 import "./podium.css"
 
 export function Podium() {
-  return <div class="podium">
-    <form class="omnisearch">
-      <input type="search" placeholder="Search" autoFocus={true} />
-      <div class="search-providers">
-        <button>MDN</button>
-        <button>NPM</button>
-      </div>
-    </form>
+  return (
+    <div class="podium">
+      <form class="omnisearch">
+        <input type="search" placeholder="Search" autoFocus={true} />
+        <div class="search-providers">
+          <button>MDN</button>
+          <button>NPM</button>
+        </div>
+      </form>
 
-    <article>
-      <section>
-        <h2>Software Development</h2>
-        <ul>
-          <li><a href="https://npmjs.com">NPM</a></li>
-          <li><a href="https://developer.mozilla.com">MDN</a></li>
-          <li><a href="https://bun.sh">Bun</a></li>
-          <li><a href="https://deno.com">Deno</a></li>
-        </ul>
-      </section>
-    </article>
-    <footer>
-    <hr/>
-      <p>Bastion is maintained by <a href="https://github.com/benchristel">Ben Christel</a>.</p>
+      <article>
+        <section>
+          <h2>Software Development</h2>
+          <ul>
+            <li>
+              <a href="https://npmjs.com">NPM</a>
+            </li>
+            <li>
+              <a href="https://developer.mozilla.com">MDN</a>
+            </li>
+            <li>
+              <a href="https://bun.sh">Bun</a>
+            </li>
+            <li>
+              <a href="https://deno.com">Deno</a>
+            </li>
+          </ul>
+        </section>
+      </article>
+      <footer>
+        <hr />
+        <p>
+          Bastion is maintained by{" "}
+          <a href="https://github.com/benchristel">Ben Christel</a>.
+        </p>
 
-  <p>The image that appears at the top of each page is <a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/23730371@N05/49396214307">619 | Elizabeth Terry | Untitled Double Landscape | Watercolor | 20x22.25</a>" by <a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/23730371@N05">theartleaguegallery</a>, and is marked with <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/publicdomain/mark/1.0/?ref=openverse">Public Domain Mark 1.0 <img src="https://mirrors.creativecommons.org/presskit/icons/pd.svg" style="height: 1em; margin-right: 0.125em; display: inline;"></img></a>.</p>
+        <p>
+          The image that appears at the top of each page is{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.flickr.com/photos/23730371@N05/49396214307"
+          >
+            619 | Elizabeth Terry | Untitled Double Landscape |
+            Watercolor | 20x22.25
+          </a>
+          " by{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.flickr.com/photos/23730371@N05"
+          >
+            theartleaguegallery
+          </a>
+          , and is marked with{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://creativecommons.org/publicdomain/mark/1.0/?ref=openverse"
+          >
+            Public Domain Mark 1.0{" "}
+            <img
+              src="https://mirrors.creativecommons.org/presskit/icons/pd.svg"
+              style="height: 1em; margin-right: 0.125em; display: inline;"
+            ></img>
+          </a>
+          .
+        </p>
 
-  <p>Theme inspired by <a href="https://www.palemoon.org/">The Pale Moon Browser</a>.</p>
-  </footer>
-  </div>
+        <p>
+          Theme inspired by{" "}
+          <a href="https://www.palemoon.org/">
+            The Pale Moon Browser
+          </a>
+          .
+        </p>
+      </footer>
+    </div>
+  )
 }
