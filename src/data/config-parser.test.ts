e5c94e73bf89e4e5c5f9cad98aa937907e410c34
Ben Christel <ben.christel@gmail.com>
2023-10-01 19:45:15 -0700

    Parses a single menu item

--- src/data/config-parser.test.ts
+++ src/data/config-parser.test.ts
@@ -10,4 +10,21 @@ test("parseConfig", {
     }
     expect(parseConfig(input), equals, expected)
   },
+
+  "parses a menu item"() {
+    const input = {
+      menu: "Foo|https://foo.com"
+    }
+    const expected = {
+      menu: [
+        {
+          text: "Foo",
+          destination: "https://foo.com",
+        },
+      ],
+      searchProviders: [],
+      directory: [],
+    }
+    expect(parseConfig(input), equals, expected)
+  },
 })
--- src/data/config-parser.ts
+++ src/data/config-parser.ts
@@ -2,8 +2,19 @@ import { Raw, Parsed } from "./config-types";
 
 export function parseConfig(raw: Raw.Config): Parsed.Config {
   return {
-    menu: [],
+    menu: parseMenu(raw.menu),
     searchProviders: [],
     directory: [],
   }
+}
+
+function parseMenu(raw: string | undefined): Array<Parsed.Link> {
+  if (!raw) {
+    return []
+  }
+  const [text, destination] = raw.split("|")
+  return [{
+    text,
+    destination,
+  }]
 }
