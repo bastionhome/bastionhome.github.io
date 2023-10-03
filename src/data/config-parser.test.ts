4e9330c760bf07dca0ebfb40cd80d980ee5e5fcd
Ben Christel <ben.christel@gmail.com>
2023-10-02 21:19:13 -0700

    Add test for empty directory

--- src/data/config-parser.test.ts
+++ src/data/config-parser.test.ts
@@ -1,93 +1,78 @@
 import {parseConfig, parseLink} from "./config-parser"
+import {HumanWritable, MachineReadable} from "./config-types"
 
-test("parseConfig", {
+test("parseConfig().menu", {
   "returns empty given empty"() {
     const input = {}
-    const expected = {
-      menu: [],
-      searchProviders: [],
-      directory: [],
-    }
-    expect(parseConfig(input), equals, expected)
+    const expected = [] as const
+    expect(parseConfig(input).menu, equals, expected)
   },
 
   "parses a menu item"() {
     const input = {
       menu: "Foo|https://foo.com",
     }
-    const expected = {
-      menu: [
-        {
-          text: "Foo",
-          destination: "https://foo.com",
-        },
-      ],
-      searchProviders: [],
-      directory: [],
-    }
-    expect(parseConfig(input), equals, expected)
+    const expected = [
+      {
+        text: "Foo",
+        destination: "https://foo.com",
+      },
+    ]
+    expect(parseConfig(input).menu, equals, expected)
   },
 
   "parses multiple menu items"() {
     const input = {
       menu: "One|https://one.com\nTwo|https://two.com",
     }
-    const expected = {
-      menu: [
-        {
-          text: "One",
-          destination: "https://one.com",
-        },
-        {
-          text: "Two",
-          destination: "https://two.com",
-        },
-      ],
-      searchProviders: [],
-      directory: [],
-    }
-    expect(parseConfig(input), equals, expected)
+    const expected = [
+      {
+        text: "One",
+        destination: "https://one.com",
+      },
+      {
+        text: "Two",
+        destination: "https://two.com",
+      },
+    ]
+    expect(parseConfig(input).menu, equals, expected)
   },
 
   "ignores blank lines"() {
     const input = {
       menu: "\n",
     }
-    const expected = {
-      menu: [],
-      searchProviders: [],
-      directory: [],
-    }
-    expect(parseConfig(input), equals, expected)
+    const expected = [] as const
+    expect(parseConfig(input).menu, equals, expected)
   },
 
   "ignores all-whitespace menu items"() {
     const input = {
       menu: " \n\t",
     }
-    const expected = {
-      menu: [],
-      searchProviders: [],
-      directory: [],
-    }
-    expect(parseConfig(input), equals, expected)
+    const expected = [] as const
+    expect(parseConfig(input).menu, equals, expected)
   },
 
   "removes spaces around link text and URLs"() {
     const input = {
       menu: " Foo | https://foo.com ",
     }
-    const expected = {
-      menu: [
-        {
-          text: "Foo",
-          destination: "https://foo.com",
-        },
-      ],
-      searchProviders: [],
-      directory: [],
-    }
-    expect(parseConfig(input), equals, expected)
+    const expected = [
+      {
+        text: "Foo",
+        destination: "https://foo.com",
+      },
+    ]
+    expect(parseConfig(input).menu, equals, expected)
+  },
+})
+
+test("parseConfig().directory", {
+  "returns empty given empty"() {
+    const input: HumanWritable.Config = {}
+    const expected = [] as Array<MachineReadable.Category>
+    expect(parseConfig(input).directory, equals, expected)
   },
 })
 
