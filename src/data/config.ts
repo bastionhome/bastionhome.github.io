bf5e153b4c749103b7cf3935c8918db4b2a255a5
Ben Christel <ben.christel@gmail.com>
2023-10-04 13:17:22 -0700

    Fix whitespace around sections

--- src/data/config.ts
+++ src/data/config.ts
@@ -10,18 +10,42 @@ export const config: HumanWritable.Config = {
   `,
   categories: [
     {
-      title: "Software Engineering",
-      entries: `
-        The Art of Unix Programming | http://www.catb.org/esr/writings/taoup/html/
-      `,
+      title: "Software Development",
       subCategories: [
         {
-          title: "web",
+          title: "Engineering",
           entries: `
-            NPM  | https://npmjs.com node package manager javascript
-            MDN  | https://developer.mozilla.com network html css javascript js
-            Bun  | https://bun.sh javascript js node
+            The Art of Unix Programming | http://www.catb.org/esr/writings/taoup/html/
+            buildingbeauty.org | https://buildingbeauty.org
+            beautiful.software | https://beautiful.software
+            refactoring.com | https://refactoring.com
+          `,
+        },
+        {
+          title: "Web Programming",
+          entries: `
+            NPM | https://npmjs.com node package manager javascript
+            MDN | https://developer.mozilla.com network html css javascript js
+            eslint.org | https://eslint.org
+            cssreference.io | https://cssreference.io
+            css-tricks.com | https://css-tricks.com
+            moderncss.dev | https://moderncss.dev
+            marked.js.org | https://marked.js.org
+            vitejs.dev | https://vitejs.dev
+            preactjs.com | https://preactjs.com
+            react.dev | https://react.dev
+            sqlite.org | https://sqlite.org
+            json.org | https://json.org
+            jwt.io | https://jwt.io
+            jsonpath.com | https://jsonpath.com
             Deno | https://deno.com javascript js node
+            Deno Docs | https://docs.deno.com
+            flow.org | https://flow.org/
+            typescriptlang.org | https://typescriptlang.org
+            iconsdb.com | https://iconsdb.com
+            mdsite | https://benchristel.github.io/mdsite
+            Can I Use | https://caniuse.com
+            Bun  | https://bun.sh javascript js node
           `,
         },
       ],
--- src/views/Category.tsx
+++ src/views/Category.tsx
@@ -9,13 +9,17 @@ type Props = {
 
 export function Category({category}: Props) {
   const {title, entries, subCategories} = category
+  const uncategorizedLinks = entries.map(directoryLink)
+
   return (
     <section class="category">
       <h2>{title}</h2>
       <div class="columns">
-        <section>
-          <ul>{entries.map(directoryLink)}</ul>
-        </section>
+        {uncategorizedLinks.length > 0 && (
+          <section>
+            <ul>{uncategorizedLinks}</ul>
+          </section>
+        )}
         {subCategories.map(({title, entries: subEntries}) => (
           <section>
             <h3>{title}</h3>
