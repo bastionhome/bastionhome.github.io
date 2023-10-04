fae3e50a466202f87d71bb42cc4b921e1ba85eda
Ben Christel <ben.christel@gmail.com>
2023-10-03 19:47:50 -0700

    Move menu nav to Podium

--- src/Controller.tsx
+++ src/Controller.tsx
@@ -1,40 +1,31 @@
-import {h, Fragment} from "preact"
-import "./app.css"
+import {h} from "preact"
 import {Podium} from "./views/Podium"
 import {parseConfig} from "./data/config-parser"
 import {config} from "./data/config"
 
 export function Controller() {
   return (
-    <>
-      <nav class="menu">
-        {parseConfig(config).menu.map((link, i) => (
-          <a href={link.destination} key={i}>
-            {link.text}
-          </a>
-        ))}
-      </nav>
-      <Podium
-        category={{
-          title: "",
-          entries: [
-            {
-              link: {
-                destination: "https://npmjs.com",
-                text: "NPM",
-              },
+    <Podium
+      menu={parseConfig(config).menu}
+      category={{
+        title: "",
+        entries: [
+          {
+            link: {
+              destination: "https://npmjs.com",
+              text: "NPM",
             },
-            {
-              link: {
-                destination: "https://developer.mozilla.com",
-                text: "MDN",
-              },
+          },
+          {
+            link: {
+              destination: "https://developer.mozilla.com",
+              text: "MDN",
             },
-            {link: {destination: "https://bun.sh", text: "Bun"}},
-            {link: {destination: "https://deno.com", text: "Deno"}},
-          ].map((entry) => entry.link),
-        }}
-      />
-    </>
+          },
+          {link: {destination: "https://bun.sh", text: "Bun"}},
+          {link: {destination: "https://deno.com", text: "Deno"}},
+        ].map((entry) => entry.link),
+      }}
+    />
   )
 }
--- src/views/Podium.tsx
+++ src/views/Podium.tsx
@@ -1,74 +1,88 @@
-import {h} from "preact"
+import {h, Fragment} from "preact"
 import "./podium.less"
 import {Category} from "./Category"
 import {ViewParams} from "./view-params"
 
 type Props = {
   category: ViewParams.Category
+  menu: Array<ViewParams.Link>
 }
 
 export function Podium(props: Props) {
   return (
-    <div class="podium">
-      <form class="omnisearch">
-        <input type="search" placeholder="Search" autoFocus={true} />
-        <div class="search-providers">
-          <button>MDN</button>
-          <button>NPM</button>
-        </div>
-      </form>
+    <>
+      <nav class="menu">
+        {props.menu.map((link, i) => (
+          <a href={link.destination} key={i}>
+            {link.text}
+          </a>
+        ))}
+      </nav>
+      <div class="podium">
+        <form class="omnisearch">
+          <input
+            type="search"
+            placeholder="Search"
+            autoFocus={true}
+          />
+          <div class="search-providers">
+            <button>MDN</button>
+            <button>NPM</button>
+          </div>
+        </form>
 
-      <article>
-        <Category category={props.category} />
-      </article>
-      <footer>
-        <hr />
-        <p>
-          Bastion is maintained by{" "}
-          <a href="https://github.com/benchristel">Ben Christel</a>.
-        </p>
+        <article>
+          <Category category={props.category} />
+        </article>
+        <footer>
+          <hr />
+          <p>
+            Bastion is maintained by{" "}
+            <a href="https://github.com/benchristel">Ben Christel</a>.
+          </p>
 
-        <p>
-          The image that appears at the top of each page is{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.flickr.com/photos/23730371@N05/49396214307"
-          >
-            619 | Elizabeth Terry | Untitled Double Landscape |
-            Watercolor | 20x22.25
-          </a>
-          " by{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.flickr.com/photos/23730371@N05"
-          >
-            theartleaguegallery
-          </a>
-          , and is marked with{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://creativecommons.org/publicdomain/mark/1.0/?ref=openverse"
-          >
-            Public Domain Mark 1.0{" "}
-            <img
-              src="https://mirrors.creativecommons.org/presskit/icons/pd.svg"
-              style="height: 1em; margin-right: 0.125em; display: inline;"
-            ></img>
-          </a>
-          .
-        </p>
+          <p>
+            The image that appears at the top of each page is{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.flickr.com/photos/23730371@N05/49396214307"
+            >
+              619 | Elizabeth Terry | Untitled Double Landscape |
+              Watercolor | 20x22.25
+            </a>
+            " by{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.flickr.com/photos/23730371@N05"
+            >
+              theartleaguegallery
+            </a>
+            , and is marked with{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://creativecommons.org/publicdomain/mark/1.0/?ref=openverse"
+            >
+              Public Domain Mark 1.0{" "}
+              <img
+                src="https://mirrors.creativecommons.org/presskit/icons/pd.svg"
+                style="height: 1em; margin-right: 0.125em; display: inline;"
+              ></img>
+            </a>
+            .
+          </p>
 
-        <p>
-          Theme inspired by{" "}
-          <a href="https://www.palemoon.org/">
-            The Pale Moon Browser
-          </a>
-          .
-        </p>
-      </footer>
-    </div>
+          <p>
+            Theme inspired by{" "}
+            <a href="https://www.palemoon.org/">
+              The Pale Moon Browser
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
+    </>
   )
 }
