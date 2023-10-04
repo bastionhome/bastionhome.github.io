82ef98f4c21f2c356adfeaea4d62eed2cc423cd4
Ben Christel <ben.christel@gmail.com>
2023-10-04 10:34:26 -0700

    Factor out entryMatches

--- src/search.ts
+++ src/search.ts
@@ -1,12 +1,24 @@
-import { curry } from "@benchristel/taste";
-import { MachineReadable } from "./data/config-types";
+import {curry} from "@benchristel/taste"
+import {MachineReadable} from "./data/config-types"
 
 type Category = MachineReadable.Category
+type Entry = MachineReadable.Entry
 
-export function search(query: string, categories: Category[]): Category[] {
+export function search(
+  query: string,
+  categories: Category[],
+): Category[] {
   return categories.filter(categoryMatches(query))
 }
 
-const categoryMatches = curry((query: string, category: Category): boolean => {
-  return category.entries.some((entry) => entry.link.destination.includes(query))
-}, "categoryMatches")
+const categoryMatches = curry(
+  (query: string, category: Category): boolean =>
+    category.entries.some(entryMatches(query)),
+  "categoryMatches",
+)
+
+const entryMatches = curry(
+  (query: string, entry: Entry): boolean =>
+    entry.link.destination.includes(query),
+  "entryMatches",
+)
