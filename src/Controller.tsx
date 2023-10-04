ed14e0c46cf5dd4b140c29edfc81bc0249f377d3
Ben Christel <ben.christel@gmail.com>
2023-10-03 20:50:04 -0700

    Add a real category to config.ts and drive the view from that

--- src/Controller.tsx
+++ src/Controller.tsx
@@ -4,27 +4,13 @@ import {parseConfig} from "./data/config-parser"
 import {config} from "./data/config"
 
 export function Controller() {
+  const category = parseConfig(config).directory[0]
   return (
     <MainView
       menu={parseConfig(config).menu}
       category={{
-        title: "",
-        entries: [
-          {
-            link: {
-              destination: "https://npmjs.com",
-              text: "NPM",
-            },
-          },
-          {
-            link: {
-              destination: "https://developer.mozilla.com",
-              text: "MDN",
-            },
-          },
-          {link: {destination: "https://bun.sh", text: "Bun"}},
-          {link: {destination: "https://deno.com", text: "Deno"}},
-        ].map((entry) => entry.link),
+        title: category.title,
+        entries: category.entries.map((e) => e.link),
       }}
     />
   )
--- src/data/config.ts
+++ src/data/config.ts
@@ -8,4 +8,15 @@ export const config: HumanWritable.Config = {
     Gmail   | https://mail.google.com
     Call    | https://benchristel.github.io/call
   `,
+  directory: [
+    {
+      title: "Software Engineering",
+      entries: `
+        NPM  | https://npmjs.com node package manager javascript
+        MDN  | https://developer.mozilla.com network html css javascript js
+        Bun  | https://bun.sh javascript js node
+        Deno | https://deno.com javascript js node
+      `,
+    },
+  ],
 }
