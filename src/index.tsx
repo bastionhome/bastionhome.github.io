4a1402c65cce4c551140180533ee4f783e3fb50a
Ben Christel <ben.christel@gmail.com>
2023-10-03 19:38:33 -0700

    Move CSS for page layout to index.html

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import {render, h} from "preact"
 import {Controller} from "./Controller"
 import "./test-results"
-import "./reset.css"
 import "./global-types"
 
 render(<Controller />, document.getElementById("preact-root")!)
